b3a1a963fab2d87eff8e0ac072a804f5c9b8916f
JSJBDEV <acrodynasty@gmail.com>
2026-01-01 23:00:12 +0000

    fixed the cannon crash again again

--- common/src/main/java/ace/actually/pirates/blocks/entity/CannonPrimingBlockEntity.java
+++ common/src/main/java/ace/actually/pirates/blocks/entity/CannonPrimingBlockEntity.java
@@ -94,9 +94,9 @@ public class CannonPrimingBlockEntity extends BlockEntity {
                 null);
 
         BlockHitResult result = world.clip(context);
-        if(VSGameUtilsKt.isBlockInShipyard(world, result.getBlockPos()))
+        ServerShip thisShip = VSGameUtilsKt.getShipManagingPos(world,result.getBlockPos());
+        if(thisShip!=null)
         {
-            ServerShip thisShip = VSGameUtilsKt.getShipManagingPos(world,result.getBlockPos());
             PlayerTeam team = world.getScoreboard().getPlayersTeam(thisShip.getSlug());
             if(team!=null)
             {
